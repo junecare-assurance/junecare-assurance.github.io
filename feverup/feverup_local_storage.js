400e59cd5be06454893bc766f7f5799a711a80df
junecare-assurance <igor@junecare.fr>
2024-10-16 10:56:52 +0200

    add local storage for onepark and feverup

---
+++ feverup/feverup_local_storage.js
@@ -0,0 +1,67 @@
+(function() {
+    'use strict';
+
+    console.log('Tampermonkey script loaded');
+
+    function saveToLocalStorage(eventInfo) {
+        console.log('Saving event info to local storage:', eventInfo);
+        localStorage.setItem('eventInfo', JSON.stringify(eventInfo));
+    }
+
+    function getEventInfo() {
+        console.log('Extracting event info');
+        const nameElement = document.querySelector('.purchase-plan-resume__title');
+        const dateElement = document.querySelector('[data-testid="purchase-plan-date"] .plan-resume-list__info div');
+        const placeElement = document.querySelector('[data-testid="purchase-plan-place"] .plan-resume-list__info div');
+        const priceElement = document.querySelector('[data-testid="purchase-plan-session"] .plan-resume-list__info div');
+
+        const name = nameElement ? nameElement.innerText : 'Non trouvé';
+        const date = dateElement ? dateElement.innerText : 'Non trouvé';
+        const place = placeElement ? placeElement.innerText : 'Non trouvé';
+        const priceText = priceElement ? priceElement.innerText : 'Non trouvé';
+
+        // Extraction du nombre de billets et du prix final
+        const priceMatch = priceText.match(/x (\d+)/);
+        const numberOfTickets = priceMatch ? priceMatch[1] : 'Non trouvé';
+        const finalPriceElements = document.querySelectorAll('span');
+        let finalPrice = 'Non trouvé';
+
+        finalPriceElements.forEach(element => {
+            const priceText = element.innerText.trim().replace(',', '.').replace('\u00A0€', '');
+            if (!isNaN(parseFloat(priceText))) {
+                finalPrice = priceText;
+            }
+        });
+
+        console.log(finalPrice);
+
+
+        return { name, date, place, numberOfTickets, finalPrice };
+    }
+
+
+    function getEmailAfterClick() {
+        const triggerElement = document.querySelector('.fv-login__avatar');
+        if (triggerElement) {
+            const interval = setInterval(() => {
+                const emailElement = document.querySelector('[data-testid="user-email-profile"]');
+                if (emailElement) {
+                    const email = emailElement.innerText || emailElement.textContent;
+                    console.log('ICI');
+                    const eventInfo = getEventInfo();
+                    eventInfo.email = email;
+                    saveToLocalStorage(eventInfo);
+                    clearInterval(interval);
+                }
+            }, 100);
+        } else {
+            console.log('Avatar not found, retrying...');
+            setTimeout(getEmailAfterClick, 1000);
+        }
+    }
+
+    window.addEventListener('load', () => {
+        console.log('Page loaded');
+        getEmailAfterClick();
+    });
+})();
---
+++ onepark/onepark_local_storage.js
@@ -0,0 +1,67 @@
+(function() {
+    'use strict';
+
+    console.log('Tampermonkey script loaded');
+
+    function saveToLocalStorage(eventInfo) {
+        console.log('Saving event info to local storage:', eventInfo);
+        localStorage.setItem('eventInfo', JSON.stringify(eventInfo));
+    }
+
+    function getEventInfo() {
+        console.log('Extracting event info');
+        const nameElement = document.querySelector('.park-card__title');
+        const dateElements = document.querySelectorAll('.park-offer-summary__dates .date');
+        const placeElement = document.querySelector('.park-card__address');
+        const priceElement = document.querySelector('.amount__price.amount__price--right span');
+        const licensePlateElement = document.querySelector('#licensePlateField');
+
+        const name = nameElement ? nameElement.innerText : 'Non trouvé';
+        const date = dateElements.length > 0 ? dateElements[0].innerHTML.replace('<br>', ' ') : 'Non trouvé';
+        const date_end = dateElements.length > 1 ? dateElements[1].innerHTML.replace('<br>', ' ') : 'Non trouvé';
+        const place = placeElement ? placeElement.innerHTML.replace('<br>', ', ').replace(',,', ',') : 'Non trouvé';
+        const price = priceElement ? priceElement.innerText.replace('\u00A0€', '').replace(',', '.') : 'Non trouvé';
+        const imat = licensePlateElement ? licensePlateElement.value : 'Non trouvé';
+
+        return { name, date, date_end, place, numberOfTickets: '1', finalPrice: price, imat };
+    }
+
+    function getEmailAndName() {
+        const emailElement = document.querySelector('.purchase-summary__email');
+        const nameElement = document.querySelector('.purchase-summary__info-element');
+
+        const email = emailElement ? emailElement.innerText : 'Non trouvé';
+        const name = nameElement ? nameElement.innerText : 'Non trouvé';
+
+        const eventInfo = getEventInfo();
+        eventInfo.email = email;
+        eventInfo.lastname = name;
+
+        saveToLocalStorage(eventInfo);
+    }
+
+    function observeLicensePlate() {
+        const licensePlateElement = document.querySelector('#licensePlateField');
+        if (licensePlateElement) {
+            const observer = new MutationObserver(() => {
+                console.log('License plate changed');
+                const eventInfo = getEventInfo();
+                saveToLocalStorage(eventInfo);
+            });
+
+            observer.observe(licensePlateElement, { attributes: true, attributeFilter: ['value'] });
+
+            licensePlateElement.addEventListener('input', () => {
+                console.log('License plate input event');
+                const eventInfo = getEventInfo();
+                saveToLocalStorage(eventInfo);
+            });
+        }
+    }
+
+    window.addEventListener('load', () => {
+        console.log('Page loaded');
+        getEmailAndName();
+        observeLicensePlate();
+    });
+})();
