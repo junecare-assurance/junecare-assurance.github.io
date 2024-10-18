a6957b297d4daaf7b01f426397b08017b6fad9d9
junecare-assurance <igor@junecare.fr>
2024-10-18 10:58:01 +0200

    fix(papvacances_encart): update style encart

--- papvacances/papvacances_encart.js
+++ papvacances/papvacances_encart.js
@@ -3,18 +3,22 @@
     function ajouterEncartAssurance() {
         const parentDiv = document.getElementById('details-tarifs-et-disponibilites');
         if (parentDiv) {
+            const hrElement = document.createElement('hr');
+            hrElement.className = 'hr-big hr-yellow no-margin-bottom';
+            
             const encartAssurance = document.createElement('div');
             encartAssurance.id = 'encart-assurance';
+            encartAssurance.className = 'bg-grey-1 padding-30 margin-bottom-30';
     
             encartAssurance.innerHTML = `
-                <hr class="hr-big hr-yellow no-margin-bottom">
                 <h3>Assurance</h3>
                 <p>Ajoutez une assurance pour une meilleure couverture.</p>
             `;
             parentDiv.insertAdjacentElement('afterend', encartAssurance);
+            parentDiv.insertAdjacentElement('afterend', hrElement);
         } else {
             console.error('Div avec id "details-tarifs-et-disponibilites" non trouvée.');
         }
     }
-    ajouterEncartAssurance();    
+    ajouterEncartAssurance();      
 })();
