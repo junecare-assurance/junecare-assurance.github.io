3e7a538490bd955ecc769a55e090b524f65ac06d
junecare-assurance <igor@junecare.fr>
2024-10-18 10:27:28 +0200

    test(onepark_storage): min price

--- onepark/onepark_local_storage.js
+++ onepark/onepark_local_storage.js
@@ -24,7 +24,10 @@
         const place = placeElement ? placeElement.innerHTML.replace('<br>', ', ').replace(',,', ',') : 'Non trouvé';
         const price = priceElement ? priceElement.innerText.replace('\u00A0€', '').replace(',', '.') : 'Non trouvé';
         const imat = licensePlateElement ? licensePlateElement.value : 'Non trouvé';
-
+        if (price < 6.25)
+        {
+            price = 6.25;
+        }
         return { name, date, date_end, place, numberOfTickets: '1', finalPrice: price, imat };
     }
 
