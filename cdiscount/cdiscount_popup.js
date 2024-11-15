2647c546be37860689e5140ac6931960f66c69e3
junecare-assurance <igor@junecare.fr>
2024-11-15 15:32:25 +0100

    test desespoire

--- cdiscount/cdiscount_popup.js
+++ cdiscount/cdiscount_popup.js
@@ -170,7 +170,7 @@ if (!window.location.href.match(/#.*$/)) {
                             });
                             popup.innerHTML = data;
 
-                        }, 100); // Attendre 100 ms pour s'assurer que le DOM est complètement chargé
+                        }, 1000); // Attendre 100 ms pour s'assurer que le DOM est complètement chargé
 
                         popup.innerHTML = data;
                     })
